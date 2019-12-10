cf83216f08c0e62a6233f97e600f91ab72b62241
LangJin <fenyukuang@163.com>
2019-12-10 14:13:03 +0800

    db update

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ __author__ = 'LangJin'
 
 
 db_config = {
-    'host': '192.144.148.91',
+    'host': '192.168.0.103',
     'port': 3306,
     'user': 'root',
-    'password': '1qaz!QAZ',
+    'password': '123456',
     'db': 'ljtestdb',
     'charset': 'utf8mb4'
 }
