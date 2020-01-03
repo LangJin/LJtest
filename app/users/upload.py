ba524fd54e5f3b75d00dc72b4919902ba52807da
LangJin <fenyukuang@163.com>
2020-01-03 16:38:04 +0800

    updateaddtoken

--- app/users/upload.py
+++ app/users/upload.py
@@ -1,4 +1,4 @@
-from flask import request,url_for,send_from_directory
+from flask import request,url_for,send_from_directory,session
 from . import userbp
 from werkzeug import secure_filename
 import os
@@ -13,19 +13,23 @@ def fileupload():
     '''
     上传图片接口，支持'png', 'jpg', 'jpeg', 'gif'格式。
     '''
-    file = request.files["file"]
-    if file and ('.' in file.filename and \
-           file.filename.rsplit('.', 1)[1] in set(['png', 'jpg', 'jpeg', 'gif'])):
-        filename = secure_filename(file.filename)
-        fname = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(15))
-        filename = fname+"."+filename.split(".")[1]
-        uploadhost = os.path.join(os.getcwd()+upload_folder, filename)
-        file.save(uploadhost)
-        # file_url = url_for("static",filename='images/uploads/'+filename)
-        return setcors(data=filename,status=200)
+    token = request.headers.get("token")
+    loginstatus = checkloginstatus(session,token)
+    if loginstatus is True:
+        file = request.files["file"]
+        if file and ('.' in file.filename and \
+            file.filename.rsplit('.', 1)[1] in set(['png', 'jpg', 'jpeg', 'gif'])):
+            filename = secure_filename(file.filename)
+            fname = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(15))
+            filename = fname+"."+filename.split(".")[1]
+            uploadhost = os.path.join(os.getcwd()+upload_folder, filename)
+            file.save(uploadhost)
+            # file_url = url_for("static",filename='images/uploads/'+filename)
+            return setcors(data=filename,status=200)
+        else:
+            return setcors(msg="请上传正确的图片。")
     else:
-        return setcors(msg="请上传正确的图片。")
-
+        return setcors(msg=loginstatus)
 
 
 @userbp.route('/imgs')
